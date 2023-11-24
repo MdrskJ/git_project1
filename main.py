272f515b12ad7fae974c77e53b001cf2b7416dd9
MdrskJ <modorskiy.jakov@gmail.com>
2023-11-24 18:57:02 +0500

    New branch is updated

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+from random import randint
+
+
+def rand():
+    print(randint(0, 1100))
+
+
 def main():
     print('My first git program')
     print('And I change it every day')
     print('I create a new branch')
+    rand()
 
 
 if __name__ == '__main__':
